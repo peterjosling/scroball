f81185d6bc2cf6992170836d2fe2d17429f2ccca
Peter Josling <peterjosling@gmail.com>
2017-04-15 22:08:45 +0100

    Show history view in tab in main activity

--- app/src/main/java/com/peterjosling/scroball/MainActivity.java
+++ app/src/main/java/com/peterjosling/scroball/MainActivity.java
@@ -5,23 +5,60 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.common.collect.ImmutableList;
+
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
   private ScroballApplication application;
+
+  /**
+   * The {@link android.support.v4.view.PagerAdapter} that will provide
+   * fragments for each of the sections. We use a
+   * {@link FragmentPagerAdapter} derivative, which will keep every
+   * loaded fragment in memory. If this becomes too memory intensive, it
+   * may be best to switch to a
+   * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+   */
+  private SectionsPagerAdapter mSectionsPagerAdapter;
+
+  /**
+   * The {@link ViewPager} that will host the section contents.
+   */
+  private ViewPager mViewPager;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    application = (ScroballApplication) getApplication();
+
     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
-    application = (ScroballApplication) getApplication();
+    // Create the adapter that will return a fragment for each of the three
+    // primary sections of the activity.
+    mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+    // Set up the ViewPager with the sections adapter.
+    mViewPager = (ViewPager) findViewById(R.id.container);
+    mViewPager.setAdapter(mSectionsPagerAdapter);
+
+    TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+    tabLayout.setupWithViewPager(mViewPager);
   }
+
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
@@ -59,5 +96,39 @@ public class MainActivity extends AppCompatActivity {
           }
         })
         .setNegativeButton(android.R.string.no, null).show();
+  }
+
+  /**
+   * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+   * one of the sections/tabs/pages.
+   */
+  public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    List<Fragment> fragments = ImmutableList.of((Fragment) new ScrobbleHistoryFragment());
+
+    public SectionsPagerAdapter(FragmentManager fm) {
+      super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+      // getItem is called to instantiate the fragment for the given page.
+      // Return a PlaceholderFragment (defined as a static inner class below).
+      return fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+      return fragments.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+      switch (position) {
+        case 0:
+          return "History";
+      }
+      return null;
+    }
   }
 }
