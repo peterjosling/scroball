e3887f0189761b020f785a696bc5159bdb1d60a9
Peter Josling <peterjosling@gmail.com>
2017-04-17 19:27:04 +0100

    Handle player (un)registration if toggled in settings

--- app/src/main/java/com/peterjosling/scroball/ListenerService.java
+++ app/src/main/java/com/peterjosling/scroball/ListenerService.java
@@ -14,6 +14,9 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 
+import com.google.common.base.Predicate;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Iterables;
 import com.google.common.collect.Sets;
 
 import java.util.ArrayList;
@@ -24,7 +27,8 @@ import java.util.Set;
 import java.util.WeakHashMap;
 
 public class ListenerService extends NotificationListenerService
-    implements MediaSessionManager.OnActiveSessionsChangedListener {
+    implements MediaSessionManager.OnActiveSessionsChangedListener,
+    SharedPreferences.OnSharedPreferenceChangeListener {
 
   private static final String TAG = ListenerService.class.getName();
 
@@ -75,6 +79,7 @@ public class ListenerService extends NotificationListenerService
     List<MediaController> initialSessions = mediaSessionManager.getActiveSessions(componentName);
     onActiveSessionsChanged(initialSessions);
 
+    sharedPreferences.registerOnSharedPreferenceChangeListener(this);
   }
 
   public static boolean isNotificationAccessEnabled(Context context) {
@@ -84,19 +89,25 @@ public class ListenerService extends NotificationListenerService
 
   @Override
   public void onActiveSessionsChanged(List<MediaController> activeMediaControllers) {
-    Set<MediaController> existingControllers = new HashSet<>(mediaControllers);
     Log.i(TAG, "Active MediaSessions changed");
 
+    Set<MediaController> existingControllers =
+        ImmutableSet.copyOf(
+            Iterables.filter(mediaControllers, new Predicate<MediaController>() {
+              @Override
+              public boolean apply(MediaController input) {
+                return controllerCallbacks.containsKey(input);
+              }
+            }));
     Set<MediaController> newControllers = new HashSet<>(activeMediaControllers);
 
     Set<MediaController> toRemove = Sets.difference(existingControllers, newControllers);
     Set<MediaController> toAdd = Sets.difference(newControllers, existingControllers);
 
     for (MediaController controller : toRemove) {
-      if (controllerCallbacks.containsKey(controller)) {
-        controller.unregisterCallback(controllerCallbacks.get(controller));
-        playbackTracker.handleSessionTermination(controller.getPackageName());
-      }
+      controller.unregisterCallback(controllerCallbacks.get(controller));
+      playbackTracker.handleSessionTermination(controller.getPackageName());
+      controllerCallbacks.remove(controller);
     }
 
     for (final MediaController controller : toAdd) {
@@ -139,6 +150,32 @@ public class ListenerService extends NotificationListenerService
     }
 
     mediaControllers = activeMediaControllers;
+  }
+
+  @Override
+  public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+    if (key.startsWith("player.")) {
+      final String packageName = key.substring(7);
+
+      if (sharedPreferences.getBoolean(key, true)) {
+        Log.i(TAG, "Player enabled, re-registering callbacks");
+        onActiveSessionsChanged(mediaControllers);
+      } else {
+        Log.i(TAG, "Player disabled, stopping any current tracking");
+        final MediaController controller = Iterables.find(mediaControllers, new Predicate<MediaController>() {
+          @Override
+          public boolean apply(MediaController input) {
+            return input.getPackageName().equals(packageName);
+          }
+        });
+
+        if (controller != null && controllerCallbacks.containsKey(controller)) {
+          controller.unregisterCallback(controllerCallbacks.get(controller));
+          playbackTracker.handleSessionTermination(controller.getPackageName());
+          controllerCallbacks.remove(controller);
+        }
+      }
+    }
   }
 
   private void controllerPlaybackStateChanged(MediaController controller, PlaybackState state) {
