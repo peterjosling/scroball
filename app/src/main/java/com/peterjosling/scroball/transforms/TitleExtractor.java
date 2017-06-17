c3b0d6cf2160a07aba1365280341c5748cc20b04
Peter Josling <peterjosling@gmail.com>
2017-06-17 22:23:53 +0100

    Fix TitleExtractor adding extra | to unmatched titles
    Java's String#split(String) takes a regex, not a simple string to
    match, so "|" splits on "" or "" (splits every character), and then the
    string gets concatenated back together with | between every character.

--- app/src/main/java/com/peterjosling/scroball/transforms/TitleExtractor.java
+++ app/src/main/java/com/peterjosling/scroball/transforms/TitleExtractor.java
@@ -4,6 +4,7 @@ import com.google.common.base.Joiner;
 import com.peterjosling.scroball.Track;
 
 import java.util.Arrays;
+import java.util.regex.Pattern;
 
 public class TitleExtractor implements MetadataTransform {
 
@@ -16,7 +17,7 @@ public class TitleExtractor implements MetadataTransform {
     String artist = null;
 
     for (String separator : SEPARATORS) {
-      String[] components = track.track().split(separator);
+      String[] components = track.track().split(Pattern.quote(separator));
 
       if (components.length > 1) {
         String[] titleComponents = Arrays.copyOfRange(components, 1, components.length);
